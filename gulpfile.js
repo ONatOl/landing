2c9742e7add04aee1fcd5dd3ea6379569c381c55
Natalia Ostiak <natalya.sosnovsckaya@gmail.com>
2018-05-18 02:51:08 +0300

    Navigation was done. Open and close functionality was added to form. Simple validation was added

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ const sass = require('gulp-sass');
 const spritesmith = require('gulp.spritesmith');
 const rimraf = require('rimraf');
 const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
 
 /* ----------- Server ------------ */
 gulp.task('server', function() {
@@ -14,7 +17,6 @@ gulp.task('server', function() {
             baseDir: "build"
         }
     });
-
 
     gulp.watch('build/**/*').on('change', browserSync.reload);
 });
@@ -36,6 +38,22 @@ gulp.task('styles:compile', function () {
     .pipe(gulp.dest('build/css'));
 });
 
+/* --------  js -------- */
+gulp.task('js', function() {
+    return gulp.src([
+        'source/js/init.js',
+        'source/js/validation.js',
+        'source/js/form.js',
+        'source/js/navigation.js',
+        'source/js/main.js'
+    ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/js'));
+});
+
 /* ---------------- Sprite -----------*/
 gulp.task('sprite', function (cb) {
   const spriteData = gulp.src('source/images/icons/*.png').pipe(spritesmith({
@@ -43,6 +61,7 @@ gulp.task('sprite', function (cb) {
     imgPath: '../images/sprite.png',
     cssName: 'sprite.scss'
   }));
+
   spriteData.img.pipe(gulp.dest('build/images/'));
   spriteData.css.pipe(gulp.dest('source/styles/global/'));
   cb();
@@ -72,11 +91,12 @@ gulp.task('copy', gulp.parallel('copy:fonts', 'copy:images'));
 gulp.task('watch', function() {
     gulp.watch('source/template/**/*.pug', gulp.series('templates:compile'));
     gulp.watch('source/styles/**/*.scss', gulp.series('styles:compile'));
+    gulp.watch('source/js/**/*.js', gulp.series('js'));
 });
 
 gulp.task('default', gulp.series(
   'clean',
-  gulp.parallel('templates:compile', 'styles:compile', 'sprite', 'copy'),
+  gulp.parallel('templates:compile', 'styles:compile', 'js', 'sprite', 'copy'),
   gulp.parallel('watch', 'server')
   )
 );
